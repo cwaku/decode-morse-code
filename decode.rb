3034f03713d1755a3d659e00abe1b352e163a900
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-03-02 19:40:09 +0100

    Add Solution

--- decode.rb
+++ decode.rb
@@ -42,3 +42,20 @@ def decode_char(code)
   decoder[code]
 end
 
+puts decode_char(".-")
+
+# Create a method to decode an entire word in Morse code, takes a string parameter, and return the string representation. Every character in a word will be separated by a single space (e.g. decode_word("-- -.--") returns "MY").
+def decode_word(code)
+  code.split(" ").map { |char| decode_char(char) }.join()
+end
+
+puts decode_word("-- -.--")
+
+# Create a method to decode the entire message in Morse code, takes a string parameter, and return the string representation. Every word will be separated by 3 spaces (e.g.
+#   decode("-- -.--   -. .- -- .") returns "MY NAME")
+
+def decode(code)
+  code.split("   ").map { |word| decode_word(word) }.join(" ")
+end
+
+puts decode("-- -.--   -. .- -- .")
